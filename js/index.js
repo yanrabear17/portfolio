842ed08703b830e4aaa035a5407ce5fc9c82bca0
yanrabears17 <yanrabears@gmail.com>
2023-07-22 06:59:18 +0800

    update responsive

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const toggleBtn = document.querySelector("#toggle_btn");
+const dropdownMenu = document.querySelector("#dropdown_menu");
+const navbarsMenu = document.querySelector(".bx-menu");
+
+toggleBtn.addEventListener("click", () => {
+  //   dropdownMenu.classList.add("open");
+  if (navbarsMenu.classList.contains("bx-menu")) {
+    navbarsMenu.classList.remove("bx-menu");
+    navbarsMenu.classList.add("bx-x");
+    dropdownMenu.classList.add("open");
+  } else {
+    dropdownMenu.classList.remove("open");
+    navbarsMenu.classList.remove("bx-x");
+    navbarsMenu.classList.add("bx-menu");
+  }
+});
+
+addEventListener("resize", () => {
+  if (window.innerWidth > 992) {
+    if (dropdownMenu.classList.contains("open")) {
+      dropdownMenu.classList.remove("open");
+      navbarsMenu.classList.remove("bx-x");
+      navbarsMenu.classList.add("bx-menu");
+    }
+  }
+});
